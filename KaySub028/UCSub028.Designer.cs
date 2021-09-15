f745ec95a1c2b03f27f04b66be7830a7c3084a99
ahyeong <dkdud081@gmail.com>
2021-09-15 17:12:36 +0900

    추이 진행중

--- KaySub028/UCSub028.Designer.cs
+++ KaySub028/UCSub028.Designer.cs
@@ -29,23 +29,26 @@
         private void InitializeComponent()
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel5 = new System.Windows.Forms.Panel();
-            this.label3 = new System.Windows.Forms.Label();
-            this.searchDept = new System.Windows.Forms.ComboBox();
-            this.dateSearch1 = new System.Windows.Forms.DateTimePicker();
-            this.label1 = new System.Windows.Forms.Label();
             this.tableLayoutPanel5 = new System.Windows.Forms.TableLayoutPanel();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.panel3 = new System.Windows.Forms.Panel();
             this.panel4 = new System.Windows.Forms.Panel();
             this.Notic = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.radiMonth = new System.Windows.Forms.RadioButton();
+            this.radiYear = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.qt_cal_date2 = new System.Windows.Forms.DateTimePicker();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.qt_cal_date1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
             this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            this.cartesianChart2 = new LiveCharts.WinForms.CartesianChart();
             this.tableLayoutPanel1.SuspendLayout();
-            this.panel2.SuspendLayout();
             this.tableLayoutPanel5.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -65,66 +68,6 @@
             this.tableLayoutPanel1.Size = new System.Drawing.Size(792, 584);
             this.tableLayoutPanel1.TabIndex = 3;
             // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.panel5);
-            this.panel2.Controls.Add(this.label3);
-            this.panel2.Controls.Add(this.searchDept);
-            this.panel2.Controls.Add(this.dateSearch1);
-            this.panel2.Controls.Add(this.label1);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(3, 3);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(786, 34);
-            this.panel2.TabIndex = 1;
-            // 
-            // panel5
-            // 
-            this.panel5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.panel5.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel5.Location = new System.Drawing.Point(0, 33);
-            this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(786, 1);
-            this.panel5.TabIndex = 45;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(167, 6);
-            this.label3.Margin = new System.Windows.Forms.Padding(3, 7, 3, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(29, 12);
-            this.label3.TabIndex = 40;
-            this.label3.Text = "직급";
-            this.label3.Visible = false;
-            // 
-            // searchDept
-            // 
-            this.searchDept.FormattingEnabled = true;
-            this.searchDept.Location = new System.Drawing.Point(202, 2);
-            this.searchDept.Name = "searchDept";
-            this.searchDept.Size = new System.Drawing.Size(100, 20);
-            this.searchDept.TabIndex = 41;
-            this.searchDept.Visible = false;
-            // 
-            // dateSearch1
-            // 
-            this.dateSearch1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateSearch1.Location = new System.Drawing.Point(61, 2);
-            this.dateSearch1.Name = "dateSearch1";
-            this.dateSearch1.Size = new System.Drawing.Size(100, 21);
-            this.dateSearch1.TabIndex = 1;
-            this.dateSearch1.Value = new System.DateTime(2021, 8, 31, 0, 0, 0, 0);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(4, 6);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 12);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "조회날짜";
-            // 
             // tableLayoutPanel5
             // 
             this.tableLayoutPanel5.ColumnCount = 2;
@@ -132,7 +75,7 @@
             this.tableLayoutPanel5.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 66.66666F));
             this.tableLayoutPanel5.Controls.Add(this.dataGridView1, 0, 1);
             this.tableLayoutPanel5.Controls.Add(this.panel3, 0, 0);
-            this.tableLayoutPanel5.Controls.Add(this.cartesianChart1, 1, 1);
+            this.tableLayoutPanel5.Controls.Add(this.cartesianChart2, 1, 1);
             this.tableLayoutPanel5.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel5.Location = new System.Drawing.Point(3, 43);
             this.tableLayoutPanel5.Name = "tableLayoutPanel5";
@@ -156,7 +99,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 23;
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(255, 492);
+            this.dataGridView1.Size = new System.Drawing.Size(256, 492);
             this.dataGridView1.TabIndex = 1;
             // 
             // panel3
@@ -166,7 +109,7 @@
             this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel3.Location = new System.Drawing.Point(3, 3);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(255, 34);
+            this.panel3.Size = new System.Drawing.Size(256, 34);
             this.panel3.TabIndex = 2;
             // 
             // panel4
@@ -175,7 +118,7 @@
             this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.panel4.Location = new System.Drawing.Point(0, 33);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(255, 1);
+            this.panel4.Size = new System.Drawing.Size(256, 1);
             this.panel4.TabIndex = 45;
             // 
             // Notic
@@ -188,14 +131,105 @@
             this.Notic.Text = "기간별 입사인원 추이";
             this.Notic.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.radiMonth);
+            this.panel2.Controls.Add(this.radiYear);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.qt_cal_date2);
+            this.panel2.Controls.Add(this.panel5);
+            this.panel2.Controls.Add(this.qt_cal_date1);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(786, 34);
+            this.panel2.TabIndex = 1;
+            // 
+            // radiMonth
+            // 
+            this.radiMonth.AutoSize = true;
+            this.radiMonth.Dock = System.Windows.Forms.DockStyle.Right;
+            this.radiMonth.Location = new System.Drawing.Point(680, 0);
+            this.radiMonth.Name = "radiMonth";
+            this.radiMonth.Size = new System.Drawing.Size(47, 33);
+            this.radiMonth.TabIndex = 49;
+            this.radiMonth.TabStop = true;
+            this.radiMonth.Text = "월별";
+            this.radiMonth.UseVisualStyleBackColor = true;
+            // 
+            // radiYear
+            // 
+            this.radiYear.AutoSize = true;
+            this.radiYear.Dock = System.Windows.Forms.DockStyle.Right;
+            this.radiYear.Location = new System.Drawing.Point(727, 0);
+            this.radiYear.Name = "radiYear";
+            this.radiYear.Size = new System.Drawing.Size(59, 33);
+            this.radiYear.TabIndex = 48;
+            this.radiYear.TabStop = true;
+            this.radiYear.Text = "년도별";
+            this.radiYear.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(167, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(11, 12);
+            this.label2.TabIndex = 47;
+            this.label2.Text = "-";
+            // 
+            // qt_cal_date2
+            // 
+            this.qt_cal_date2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.qt_cal_date2.Location = new System.Drawing.Point(184, 2);
+            this.qt_cal_date2.Name = "qt_cal_date2";
+            this.qt_cal_date2.Size = new System.Drawing.Size(100, 21);
+            this.qt_cal_date2.TabIndex = 46;
+            this.qt_cal_date2.Value = new System.DateTime(2021, 8, 31, 0, 0, 0, 0);
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel5.Location = new System.Drawing.Point(0, 33);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(786, 1);
+            this.panel5.TabIndex = 45;
+            // 
+            // qt_cal_date1
+            // 
+            this.qt_cal_date1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.qt_cal_date1.Location = new System.Drawing.Point(61, 2);
+            this.qt_cal_date1.Name = "qt_cal_date1";
+            this.qt_cal_date1.Size = new System.Drawing.Size(100, 21);
+            this.qt_cal_date1.TabIndex = 1;
+            this.qt_cal_date1.Value = new System.DateTime(2021, 8, 31, 0, 0, 0, 0);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "조회날짜";
+            // 
             // cartesianChart1
             // 
-            this.cartesianChart1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.cartesianChart1.Location = new System.Drawing.Point(264, 43);
+            this.cartesianChart1.Location = new System.Drawing.Point(0, 0);
             this.cartesianChart1.Name = "cartesianChart1";
-            this.cartesianChart1.Size = new System.Drawing.Size(519, 492);
-            this.cartesianChart1.TabIndex = 3;
-            this.cartesianChart1.Text = "cartesianChart1";
+            this.cartesianChart1.Size = new System.Drawing.Size(200, 100);
+            this.cartesianChart1.TabIndex = 0;
+            // 
+            // cartesianChart2
+            // 
+            this.cartesianChart2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cartesianChart2.Location = new System.Drawing.Point(265, 43);
+            this.cartesianChart2.Name = "cartesianChart2";
+            this.cartesianChart2.Size = new System.Drawing.Size(518, 492);
+            this.cartesianChart2.TabIndex = 3;
+            this.cartesianChart2.Text = "cartesianChart2";
             // 
             // UserControl1
             // 
@@ -206,12 +240,12 @@
             this.Name = "UserControl1";
             this.Size = new System.Drawing.Size(792, 584);
             this.tableLayoutPanel1.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
             this.tableLayoutPanel5.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -227,9 +261,12 @@
         private LiveCharts.WinForms.CartesianChart cartesianChart1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel5;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.ComboBox searchDept;
-        private System.Windows.Forms.DateTimePicker dateSearch1;
+        private System.Windows.Forms.DateTimePicker qt_cal_date1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker qt_cal_date2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radiMonth;
+        private System.Windows.Forms.RadioButton radiYear;
+        private LiveCharts.WinForms.CartesianChart cartesianChart2;
     }
 }
